4e5311e00aa54f3bf2fea75ea2e9e4956932af79
John Hanot <johnhanot@gmail.com>
2024-03-15 17:46:19 +0100

    Add fugivite

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@
 -- Clear search with <F3>
 vim.keymap.set({ "i", "n" }, "<F3>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
 
-vim.keymap.set({ "i" }, "<Home>", function()
+vim.keymap.set({ "i", "n" }, "<Home>", function()
   local current_line, current_col = unpack(vim.api.nvim_win_get_cursor(0))
   local first_non_blank = vim.api.nvim_get_current_line():match("^%s*"):len() or 0
   vim.api.nvim_win_set_cursor(0, { current_line, (first_non_blank ~= current_col) and first_non_blank or 0 })
