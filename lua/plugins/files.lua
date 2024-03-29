0274d5bcfa65cc8ae8e97a4c456af5d23a13b75e
John Hanot <johnhanot@gmail.com>
2024-03-29 11:33:05 +0100

    Deactivate flash; add terraform syntax

--- lua/plugins/files.lua
+++ lua/plugins/files.lua
@@ -2,4 +2,6 @@ return {
   -- Add suda; handles writing as a sudoer
   { "lambdalisue/suda.vim", name = "suda" },
   { "tpope/vim-fugitive", name = "fugitive" },
+  { "folke/flash.nvim", enabled = false },
+  { "hashivim/vim-terraform", name = "terraform-syntax" },
 }
