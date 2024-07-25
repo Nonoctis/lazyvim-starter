b0bfa37fc8baf8921b8f462aa0e889260eb5ea80
John Hanot <johnhanot@gmail.com>
2024-07-25 16:59:04 +0200

    Add a bunch of colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,12 @@
 return {
   -- Add catppuccin
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "folke/tokyonight.nvim", lazy = false },
+  { "rose-pine/neovim", lazy = false },
+  { "craftzdog/solarized-osaka.nvim", lazy = false },
+  { "neanias/everforest-nvim", lazy = false },
+  { "rebelot/kanagawa.nvim", lazy = false },
+  { "maxmx03/solarized.nvim", lazy = false },
 
   -- Configure LazyVim to load catppuccin
   {
